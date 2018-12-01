13a49249736669915eb994bc503579a4fd56e246
Ali <tkfreak01@gmail.com>
2018-12-01 05:43:49 -0800

    Failing a test

--- week2tdd/src/main/java/School.java
+++ week2tdd/src/main/java/School.java
@@ -1,3 +1,14 @@
+import java.util.Date;
+
 public class School {
 
+    public School(String name, Date date) {
+
+    }
+
+//    public String getName() {
+//        String name="Fontys";
+//        return  name;
+//
+//    }
 }
--- week2tdd/src/test/java/SchoolTest.java
+++ week2tdd/src/test/java/SchoolTest.java
@@ -1,10 +1,19 @@
 import org.junit.Test;
 
+import java.util.Date;
+
+import static junit.framework.TestCase.assertEquals;
+
 
 public class SchoolTest {
     @Test
     public void SchoolConstructerTest(){
-        School mySchool=new School();
+        School mySchool=new School("Fontys",new Date());
+
+        //mySchool.getName();
+        String ExpectedName="Fontys";
+
+        assertEquals(ExpectedName,"Tue");
 
     }
 }
