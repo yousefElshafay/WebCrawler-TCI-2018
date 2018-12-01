79cff17678f94370297ba98b5ea2cb4709f38070
Ali <tkfreak01@gmail.com>
2018-12-01 12:22:38 -0800

    Failing Test-Date

--- week2tdd/src/main/java/School.java
+++ week2tdd/src/main/java/School.java
@@ -2,16 +2,27 @@ import java.util.Date;
 
 public class School {
     String Name;
+    Date OpeningDate;
 
-    public School(String name, Date date) {
-        this.Name=name;
-
-
+    public School(String name, Date openingDate) {
+        Name = name;
+        OpeningDate = openingDate;
     }
 
     public String getName() {
-       // String name="Fontys";
-        return  Name;
+        return Name;
+    }
 
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Date getOpeningDate() {
+        return OpeningDate;
+    }
+
+    public void setOpeningDate(Date openingDate) {
+        OpeningDate = openingDate;
     }
 }
+
--- week2tdd/src/test/java/SchoolTest.java
+++ week2tdd/src/test/java/SchoolTest.java
@@ -1,21 +1,54 @@
 import org.junit.Test;
 
+import java.util.Calendar;
 import java.util.Date;
-
+//import java.util.Calender;
 import static junit.framework.TestCase.assertEquals;
 
 
 public class SchoolTest {
     public static final String name="Fontys";
+
+  //  private final Calendar cal = Calendar.getInstance();
+
+    //public Calendar getCal() {
+//        cal.set(Calendar.YEAR, 1988);
+//        cal.set(Calendar.MONTH, Calendar.JANUARY);
+//        cal.set(Calendar.DAY_OF_MONTH, 1);
+//        Date dateRepresentation = cal.getTime();
+     //   return cal;
+
+   // }
+//    private int Y = 2015; // Year 2015
+//    private int M = 11;   // 0..11 -- December
+//    private int D = 15;   // 15th
+//    private int H = 16;   // 4:00 PM
+//    private int MN = 28;  // 4:28 PM
+//    private int S = 41;   // 4:28:41
+//    private Date d = new Date(Y-1900,M,D,H,MN,S);
+//
+//    private Calendar c = Calendar.getInstance();
+   // c.Set(Y, M, D, H, MN, S);
+
+
+
+
     @Test
 
     public void SchoolConstructerTest(){
 
-        School mySchool=new School(name,new Date());
+//        c.set(Y, M, D, H, MN, S);
+//        d=c.getTime();
+        School mySchool=new School(name,new Date(1997,01,01,00,00));
 
         //mySchool.getName();
-        String ExpectedName="Fontys";
 
+
+        String ExpectedName="Fontys";
+        Date Expecteddate=new Date(1997,01,01,00,01);
+        //Date actualDate=d.setTime(10);
+
+        assertEquals(Expecteddate,mySchool.getOpeningDate());
         assertEquals(ExpectedName,mySchool.getName());
 
     }
