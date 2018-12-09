7df0dd8fdd24c28d7fda7f08dc59ddfc4a4d7675
Mubasher Ahmed, Mubasher <m.mubasherahmed@student.fontys.nl>
2018-12-10 00:32:55 +0100

    Fail Name_date_Null_Allowed()

--- week2TCI/src/test/java/SchoolTest.java
+++ week2TCI/src/test/java/SchoolTest.java
@@ -26,13 +26,26 @@ public class SchoolTest {
             GregorianCalendar opening_date_School = new GregorianCalendar(2018, Calendar.SEPTEMBER, 3);
 
             // Act
-            school = new School("Fontys University of Applied Science", opening_date_School);
+            school = new School("Fontys  of Applied Science", opening_date_School);
             String name = school.getName();
             Date _opening_date = school.getOpeningDate();
 //
             Assert.assertTrue(!name.isEmpty() && _opening_date != null
             );
         }
+    @Test
+    public void Name_date_Null_Allowed() throws CourseDataException {
+
+        School school;
+        school = new School("Fontys University of Applied Science",
+                new GregorianCalendar(2018, Calendar.SEPTEMBER, 3));
+
+        String name = school.getName();
+        Date opening_date = school.getOpeningDate();
+
+
+        Assert.assertTrue(name == null && opening_date == null);
+    }
 
 
 
