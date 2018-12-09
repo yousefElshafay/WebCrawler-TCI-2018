8ac24ca1bdf53c5d51414a1c9a6f81fa3c97679d
EL-Shafay,Youssef Y.M.Y <y.elshafay@student.fontys.nl>
2018-12-10 00:38:02 +0100

    add schoolclass

---
+++ src/main/java/School.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+import java.util.List;
+
+public class School {
+
+    private String Schoolname ;
+    private Date openingDate;
+    private List<Course> courses ;
+
+
+
+}
