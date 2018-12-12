51005ba9e80521425be709490f24d756b564b8cb
Ali <tkfreak01@gmail.com>
2018-12-12 10:54:56 -0800

    added test class and test method

---
+++ src/test/java/UserServiceTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class UserServiceTest {
+
+    private UserService userService;
+    @Test
+    public void UserServiceMocking{
+        userService=mock(UserService.Class);//arrange
+
+
+    }
+}
