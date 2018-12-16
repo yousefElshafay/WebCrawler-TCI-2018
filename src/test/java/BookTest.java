3ee48dd6cb549faf1721222be8aff67f43456b4b
Ali <tkfreak01@gmail.com>
2018-12-16 11:51:04 -0800

    book test methodsSs

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import org.junit.Before;
+import org.junit.Test;
 
 public class BookTest {
+
+    /*setup method will be executed first before test */
+    @Before
+    public void setUp(){
+
+    }
+    /*this method will check if the book has a author or not*/
+    @Test
+    public void bookShoulHaveAuthor(){
+
+    }
+    /*this method will test if the book has a publisher*/
+    @Test
+    public void bookShouldHavePublisher(){
+
+    }
+    /*this method will test if the book has an ISBN*/
+    @Test
+    public void bookShouldHaveISBN(){
+
+    }
 }
