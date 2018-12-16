2ef53116679ed374eeca720efc1b108ce97ff439
Mubasher Ahmed, Mubasher <m.mubasherahmed@student.fontys.nl>
2018-12-16 21:54:58 +0100

    added methods into movietest class

--- src/test/java/MoviesTest.java
+++ src/test/java/MoviesTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class MoviesTest {
+
+
+    /*setup method will be executed first before test */
+    @Before
+    public void setUp(){
+
+    }
+
+    // check the director of the movie
+    @Test
+    public void shouldHaveDirector (){
+
+}
+// check the year of the movie
+    @Test
+    public void shouldHaveYear (){
+// check the writer of the movie
+    }
+    @Test
+    public void shouldHaveWriter (){
+
+    }
+
+
 }
