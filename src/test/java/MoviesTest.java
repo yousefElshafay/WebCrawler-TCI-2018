56e44efdee2922f737fc8d195c2db657bc8370ce
EL-Shafay,Youssef Y.M.Y <y.elshafay@student.fontys.nl>
2018-12-22 19:30:06 +0100

    Add movie has director test implementation

--- src/test/java/MoviesTest.java
+++ src/test/java/MoviesTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,6 +14,10 @@ public class MoviesTest {
     // check the director of the movie
     @Test
     public void shouldHaveDirector (){
+        Movies testMovie = new Movies("testmovie","testformat", 2018,"steven Spielberg",null,null);
+
+        // validating object exist
+        Assert.assertNotNull(testMovie);
 
 }
 // check the year of the movie
