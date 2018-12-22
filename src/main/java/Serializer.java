18d55ddb3079b9129a15c01b762d37da7e3bdc74
EL-Shafay,Youssef Y.M.Y <y.elshafay@student.fontys.nl>
2018-12-22 19:43:14 +0100

    Refactor project and add comments

--- src/main/java/Serializer.java
+++ src/main/java/Serializer.java
@@ -2,6 +2,8 @@ import java.util.List;
 
 public class Serializer implements ISerializer {
 
+
+    // serilize media object to a string for http response
     @Override
     public String ListOfMediaToJson(List<String> mediaItem) {
         return null;
