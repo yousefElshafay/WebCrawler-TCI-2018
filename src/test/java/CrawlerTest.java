0347e9b4f7d128a7f3c4ccbb58205c121594ccf8
Ali <tkfreak01@gmail.com>
2019-01-04 09:49:44 -0800

    Added more tests and description

--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -1,4 +1,8 @@
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
 
 public class CrawlerTest {
 
@@ -8,4 +12,46 @@ public class CrawlerTest {
 
 
     }
+
+    /*crawlerMethodForAllIsCalledOnlyOnce*/
+    @Test
+    public void getAllItemsTest() throws IOException{
+
+        //arrange
+
+        //act
+
+        //assert
+
+    }
+    /*this is to verify serializerListOfJson is called only once*/
+    @Test
+
+    public void SerlizerListToJsonTest() throws IOException{
+
+        //arrange
+
+        //act
+
+        //assert
+
+    }
+
+    /*
+      Tests for GetSpecific Item
+     */
+    /*this is to test that getStatics is called only once*/
+    @Test
+
+    public void CalledOnlyOnce() throws IOException{
+
+        //arrange
+
+        //act
+
+        //assert
+
+    }
+
+
 }
