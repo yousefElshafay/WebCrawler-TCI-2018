30b065805c86652020b549a2ecbbf7c4ee848f5a
EL-Shafay,Youssef Y.M.Y <y.elshafay@student.fontys.nl>
2019-01-09 13:26:44 +0100

    "Book Test class implemetation "

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,26 +1,30 @@
+import MediaLibrary.Books;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BookTest {
-
+    Books testbook;
     /*setup method will be executed first before tests */
     @Before
     public void setUp(){
 
+        testbook = new Books("testformat","science fiction",1993,"bill heffnton","sun","27642");
     }
     /*this method will check if the book has a author or not*/
     @Test
     public void bookShoulHaveAuthor(){
-
+        Assert.assertNotNull(testbook.getAuthor());
     }
     /*this method will test if the book has a publisher*/
     @Test
     public void bookShouldHavePublisher(){
+        Assert.assertNotNull(testbook.getPublisher());
 
     }
     /*this method will test if the book has an ISBN*/
     @Test
     public void bookShouldHaveISBN(){
-
+        Assert.assertNotNull(testbook.getIsbn());
     }
 }
